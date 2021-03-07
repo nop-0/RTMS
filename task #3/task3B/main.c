530a40cea57c429d0a1719421ce571b8c8e0430a
nop-0 <79671740+nop-0@users.noreply.github.com>
2021-03-07 19:17:10 +0800

    Add files via upload

---
+++ task #3/task3B/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <string.h>
+#include "NUC1xx.h"
+#include "GPIO.h"
+#include "SYS.h"
+#include "SPI.h"
+#include "LCD.h"
+
+#define  ONESHOT  0   // counting and interrupt when reach TCMPR number, then stop
+#define  PERIODIC 1   // counting and interrupt when reach TCMPR number, then counting from 0 again
+#define  TOGGLE   2   // keep counting and interrupt when reach TCMPR number, tout toggled (between 0 and 1)
+#define  CONTINUOUS 3 // keep counting and interrupt when reach TCMPR number
+
+volatile uint32_t ledState0 = 0; //tracks state of LED
+volatile uint32_t ledState1 = 0;
+volatile uint32_t ledState2 = 0;
+
+static uint16_t Timer0Counter=0; //sets up counter for each timer
+static uint16_t Timer1Counter=0;
+static uint16_t Timer2Counter=0;
+
+
+void InitTIMER0(void)
+{         
+	SYSCLK->CLKSEL1.TMR0_S = 0;	
+	SYSCLK->APBCLK.TMR0_EN = 1;
+	
+	TIMER0->TCSR.MODE=PERIODIC;		
+
+	TIMER0->TCSR.PRESCALE=255;	
+	TIMER0->TCMPR = 46875; // 1 second
+	
+	TIMER0->TCSR.IE = 1;
+	TIMER0->TISR.TIF = 1;		
+	NVIC_EnableIRQ(TMR0_IRQn);
+
+	TIMER0->TCSR.CRST = 1;		
+	TIMER0->TCSR.CEN = 1;		
+}
+
+void TMR0_IRQHandler(void) 
+{
+	ledState0 = ~ ledState0;  // changing ON/OFF state
+	if(ledState0)
+	{
+		DrvGPIO_ClrBit(E_GPC,12); 
+	}
+	else DrvGPIO_SetBit(E_GPC,12); 
+	
+ 	TIMER0->TISR.TIF =1;
+}
+
+void InitTIMER1(void)
+{
+	SYSCLK->CLKSEL1.TMR1_S = 0;	
+  SYSCLK->APBCLK.TMR1_EN = 1;
+
+	TIMER1->TCSR.MODE=TOGGLE;
+
+	TIMER1->TCSR.PRESCALE=255;
+	TIMER1->TCMPR = 93750;							
+
+	TIMER1->TCSR.IE = 1;
+	TIMER1->TISR.TIF = 1;		
+	NVIC_EnableIRQ(TMR1_IRQn);	
+
+	TIMER1->TCSR.CRST = 1;
+	TIMER1->TCSR.CEN = 1;
+}
+
+void TMR1_IRQHandler(void) 
+{
+	ledState1 = ~ ledState1;  // changing ON/OFF state
+	if(ledState1) 
+	{
+		DrvGPIO_ClrBit(E_GPC,13);
+	}
+	else DrvGPIO_SetBit(E_GPC,13); 
+	
+ 	TIMER1->TISR.TIF =1; 
+}
+
+void InitTIMER2(void)
+{       
+	SYSCLK->CLKSEL1.TMR2_S = 0;	
+  SYSCLK->APBCLK.TMR2_EN = 1;
+
+	TIMER2->TCSR.MODE=TOGGLE;
+
+	TIMER2->TCSR.PRESCALE=255;
+	TIMER2->TCMPR = 187500;
+							
+	TIMER2->TCSR.IE = 1;
+	TIMER2->TISR.TIF = 1;
+	NVIC_EnableIRQ(TMR2_IRQn);
+
+	TIMER2->TCSR.CRST = 1;
+	TIMER2->TCSR.CEN = 1;
+}
+
+void TMR2_IRQHandler(void) // Timer2 interrupt subroutine 
+{
+	ledState2 = ~ ledState2;  // changing ON/OFF state	
+	if(ledState2) 
+	{
+		DrvGPIO_ClrBit(E_GPC,14);
+	}
+	else DrvGPIO_SetBit(E_GPC,14);
+	
+ 	TIMER2->TISR.TIF =1;	
+}
+
+void INIT_LED()
+{
+	DrvGPIO_Open(E_GPC,12,E_IO_OUTPUT);
+	DrvGPIO_Open(E_GPC,13,E_IO_OUTPUT);
+	DrvGPIO_Open(E_GPC,14,E_IO_OUTPUT);
+
+	DrvGPIO_SetBit(E_GPC,12);
+	DrvGPIO_SetBit(E_GPC,13);
+	DrvGPIO_SetBit(E_GPC,14);
+}
+
+int main(void)
+{
+	UNLOCKREG();
+	SYSCLK->PWRCON.XTL12M_EN = 1;//Enable 12MHz Crystal
+	DrvSYS_Delay(5000); // waiting for 12MHz crystal stable
+	SYSCLK->CLKSEL0.HCLK_S = 0;
+	LOCKREG();
+	init_LCD();
+	INIT_LED();
+	clear_LCD();
+	
+	InitTIMER0();
+	InitTIMER1();
+	InitTIMER2();	
+	
+	while(1)
+	{
+	}	
+}
